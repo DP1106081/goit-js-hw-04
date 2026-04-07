2eac08613502173b56702105b4ee87c7a267b309
DP <dpkds161@gmail.com>
2026-04-07 16:14:13 +0300

    hw04

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,20 @@
-function filterArray(numbers, value) {
-  let totalNum = [];
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-  for (const item of numbers) {
-    if (item > value) {
-      totalNum.push(item);
-    }
-  }
-  return totalNum;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
